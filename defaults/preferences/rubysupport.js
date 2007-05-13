34ec7b98cf94f97f0e00f865b136ed3750d83f85
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-13 20:46:37 +0000

    拡張機能名と説明文をローカライズした
    git-svn-id: https://www.cozmixng.org/repos/piro/rubysupport/trunk@574 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ defaults/preferences/rubysupport.js
@@ -0,0 +1,2 @@
+pref("extensions.{0620B69D-7B58-416d-A92A-0198860C2757}.name", "chrome://rubysupport/locale/rubysupport.properties");
+pref("extensions.{0620B69D-7B58-416d-A92A-0198860C2757}.description", "chrome://rubysupport/locale/rubysupport.properties");
